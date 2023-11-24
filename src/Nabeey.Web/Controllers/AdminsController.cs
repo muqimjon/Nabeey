34c2b51d0eee39327422bcb52f2dd5bf2832dc4d
Muqimjon <muqimjon.mr@gmail.com>
2023-11-24 07:49:13 +0500

    Default views added

---
+++ src/Nabeey.Web/Controllers/AdminsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nabeey.Service.Interfaces;
+
+namespace Nabeey.Web.Controllers;
+
+public class AdminsController : Controller
+{
+    private readonly IUserService userService;
+    public AdminsController(IUserService userService)
+    {
+        this.userService = userService;
+    }
+
+    public async ValueTask<IActionResult> Index()
+        => View(await userService.RetrieveAllAsync());
+}
